6cafdca9ff2f41cbe452243a82c0834851e9070a
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-03-24 19:36:52 +0530

    Update Customer.java
    work from home changes done

--- BridgeLabzServlets/src/main/java/com/bridgelabz/loginreg/model/Customer.java
+++ BridgeLabzServlets/src/main/java/com/bridgelabz/loginreg/model/Customer.java
@@ -1,8 +1,17 @@
 package com.bridgelabz.loginreg.model;
+//Creating Model class of customer and creating getters and setters for that
 public class Customer {
 	private String firstName;
+	private String lastName;
 	private String userId;
 	private String password;
+    private String emailId;
+    private int phoneNumber;
+   	public Customer(String firstName2, String lastName2, String userID2, String password2, String emailId2,
+			int phoneNumber2) {}
+
+	public Customer() {
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -11,23 +20,17 @@ public class Customer {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
+	public void setlastName(String lastName) {
+		this.lastName=lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
 	public String getUserId() {
 		return userId;
 	}
-
-	public Customer() {
-		super();
-	}
-
-	public Customer(String firstName, String userId, String password) {
-		super();
-		this.firstName = firstName;
-		this.userId = userId;
-		this.password = password;
-	}
-
-	public void setUserId(String userId) {
+	
+		public void setUserId(String userId) {
 		this.userId = userId;
 	}
 
@@ -38,5 +41,18 @@ public class Customer {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	public String getEmiald() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId=emailId;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber=phoneNumber;
+	}
+	
 }
+
