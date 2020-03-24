87ab8b61ea03299d2c3f06a90a6d994a5ee51fe4
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-03-24 20:07:42 +0530

    Update LoginRegisterController.java
    changes completed

--- BridgeLabzServlets/src/main/java/com/bridgelabz/loginreg/controller/LoginRegisterController.java
+++ BridgeLabzServlets/src/main/java/com/bridgelabz/loginreg/controller/LoginRegisterController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.bridgelabz.loginreg.model.Customer;
 import com.bridgelabz.loginreg.repository.CustomerDao;
 import com.bridgelabz.loginreg.serviceprovider.CustomerDaoImpl;
