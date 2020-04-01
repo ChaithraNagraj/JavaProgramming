577e99fd412fda1c6bead74a21458564a26879a8
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-01 11:14:53 +0530

    Update UserServiceImp.java
    small changes

--- SpringRegister/src/main/java/com/springloginandregister/service/UserServiceImp.java
+++ SpringRegister/src/main/java/com/springloginandregister/service/UserServiceImp.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.soringloginregister.dao.Userdao;
 import com.springloginandregister.model.Login;
 import com.springloginandregister.model.User;
-
+/** Here we are validating the user to further Login and register move
+**/
 public class UserServiceImp  implements UserService
 {
 @Autowired
