6bfb386e1bd4c059df1f7fbc4ab2dce2f64ec96f
chaithraNagraj <56997768+chaithra-harshi@users.noreply.github.com>
2020-04-02 21:23:57 +0530

    Update DbConnection.java
    snuffy

--- LoginPageApplication/src/com/bridgelabz/repository/DbConnection.java
+++ LoginPageApplication/src/com/bridgelabz/repository/DbConnection.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class DbConnection {
 	private static Connection connection = null;
 
+	
 	/**
 	 * Purpose: method for establishing connection with the database
 	 * 
